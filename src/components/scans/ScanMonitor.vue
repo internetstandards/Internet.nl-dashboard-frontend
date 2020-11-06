18a0e60fcba370cb441aadd152d365e45c6e7d53
stitch1 <elger.jonker@gmail.com>
2020-11-06 13:00:03 +0100

    Add progress bar at scan monitor, translate scan progress

--- src/components/scans/ScanMonitor.vue
+++ src/components/scans/ScanMonitor.vue
@@ -61,8 +61,23 @@ h4 {
                             scan.list
                         }}
                     </router-link>
-                    <br>
-                    <br>
+                    <br><br>
+
+                    <template v-if="progress_bar_data(scan.log[0]['state']).state === 'error'">
+                        <b-progress :value="100" variant="danger">
+                            <b-progress-bar :value="100">
+                                <span>{{ $t('progress.' + scan.log[0]['state']) }}</span>
+                            </b-progress-bar>
+                        </b-progress>
+                    </template>
+                    <template v-else>
+                        <b-progress show-progress variant="info" :animated="progress_bar_data(scan.log[0]['state']).percentage!==100">
+                            <b-progress-bar :value="progress_bar_data(scan.log[0]['state']).percentage">
+                                <span>{{ progress_bar_data(scan.log[0]['state']).percentage }}%</span>
+                            </b-progress-bar>
+                        </b-progress>
+                    </template><br>
+
                     <template v-if="['finished', 'cancelled'].includes(scan.state)">
                         <b v-if="scan.state === 'cancelled'">{{ $t("cancelled_on") }}</b>
                         <b v-if="scan.state === 'finished'">{{ $t("finished_on") }}</b> <br>
@@ -70,6 +85,8 @@ h4 {
                                 humanize_date(scan.finished_on)
                             }},<br>{{ humanize_relative_date(scan.finished_on) }}</span><br><br>
                     </template>
+
+
                     <b>{{ $t("runtime") }}</b><br>
                     {{ humanize_duration(scan.runtime) }}<br>
                     <br>
@@ -92,7 +109,7 @@ h4 {
                         <div slot="content">
                             <ul style="font-size: 0.7em;">
                                 <li v-for="log_item in scan.log" :key="log_item.id">
-                                    - {{ log_item.state }}, {{ humanize_relative_date(log_item.at_when) }}
+                                    - {{ $t('progress.' + log_item.state) }}, {{ humanize_relative_date(log_item.at_when) }}
                                 </li>
                             </ul>
                             <template v-if="!scan.finished">
@@ -175,6 +192,37 @@ export default {
             show_stop_scan: false,
             stop_scan: null, // the scan that will be cancelled when you hit OK
             stop_scan_server_response: "",
+
+            // states of a scan, to visualize a progress bar:
+            scan_state_and_progress: {
+                "requested": {"percentage": 5, "state": "good"},
+                "discovering endpoints": {"percentage": 7, "state": "good"},
+                "discovered endpoints": {"percentage": 10, "state": "good"},
+                "retrieving scannable urls": {"percentage": 12, "state": "good"},
+                "retrieved scannable urls": {"percentage": 15, "state": "good"},
+                "registering scan at internet.nl": {"percentage": 17, "state": "good"},
+                "registered scan at internet.nl": {"percentage": 20, "state": "good"},
+                "running scan": {"percentage": 25, "state": "good"},
+                "registered": {"percentage": 25, "state": "good"},
+                "scan results ready": {"percentage": 50, "state": "good"},
+                "importing scan results": {"percentage": 60, "state": "good"},
+                "imported scan results": {"percentage": 65, "state": "good"},
+                "storing scan results": {"percentage": 70, "state": "good"},
+                "creating report": {"percentage": 75, "state": "good"},
+                "created report": {"percentage": 80, "state": "good"},
+                "sending mail": {"percentage": 90, "state": "good"},
+                "sent mail": {"percentage": 95, "state": "good"},
+                "finished": {"percentage": 100, "state": "good"},
+
+                "skipped sending mail: no mail server configured":  {"percentage": 95, "state": "good"},
+                "skipped sending mail: no e-mail addresses associated with account":  {"percentage": 95, "state": "good"},
+
+                "network_error":  {"percentage": 0, "state": "error"},
+                "configuration_error":  {"percentage": 0, "state": "error"},
+                "timeout":  {"percentage": 0, "state": "error"},
+                "error":  {"percentage": 0, "state": "error"},
+                "cancelled":  {"percentage": 100, "state": "good"},
+            }
         }
     },
     mounted: function () {
@@ -209,7 +257,14 @@ export default {
                     this.stop_stop_scan();
                 }
             );
-        }
+        },
+        progress_bar_data: function(current_scan_state) {
+            let data = this.scan_state_and_progress[current_scan_state]
+            if (data === undefined){
+                data = {percentage: 0, state: "error"}
+            }
+            return data
+        },
     }
 }
 </script>
@@ -242,6 +297,33 @@ export default {
             "list": "list",
             "ok": "Stop scan",
             "cancel": "Continue scanning"
+        },
+        "progress": {
+            "requested": "requested",
+            "discovering endpoints": "discovering endpoints",
+            "discovered endpoints": "discovered endpoints",
+            "retrieving scannable urls": "retrieving scannable urls",
+            "retrieved scannable urls": "retrieved scannable urls",
+            "registering scan at internet": {"nl": "registering scan"},
+            "registered scan at internet": {"nl": "registered scan"},
+            "running scan": "running scan",
+            "registered": "registered scan",
+            "scan results ready": "scan results ready",
+            "importing scan results": "importing scan results",
+            "imported scan results": "imported scan results",
+            "storing scan results": "storing scan results",
+            "creating report": "creating report",
+            "created report": "created report",
+            "sending mail": "sending mail",
+            "sent mail": "sent mail",
+            "finished": "finished",
+
+            "skipped sending mail: no mail server configured": "skipped sending mail: no mail server configured",
+            "skipped sending mail: no e-mail addresses associated with account": "skipped sending mail: no e-mail addresses associated with account",
+
+            "network_error": "network problem detected, contact internet.nl",
+            "configuration_error": "configuration error detected, contact internet.nl",
+            "timeout": "operation timed out, will retry"
         }
     },
     "nl": {
@@ -271,6 +353,33 @@ export default {
             "list": "lijst",
             "ok": "Stop scan",
             "cancel": "Blijf scannen"
+        },
+        "progress": {
+            "requested": "aangevraagd",
+            "discovering endpoints": "inventariseren mogelijke scans",
+            "discovered endpoints": "mogelijke scans bekend",
+            "retrieving scannable urls": "ophalen scanbare domeinen",
+            "retrieved scannable urls": "scanbare domeinen opgehaald",
+            "registering scan at internet": {"nl": "scan registreren"},
+            "registered scan at internet": {"nl": "scan geregistreerd"},
+            "running scan": "scan wordt uitgevoerd",
+            "registered": "scan geregistreerd",
+            "scan results ready": "scanresultaten beschikbaar",
+            "importing scan results": "scanresultaten importeren",
+            "imported scan results": "scanresultaten geïmporteerd",
+            "storing scan results": "scanresultaten opslaan",
+            "creating report": "rapport wordt gemaakt",
+            "created report": "rapport gemaakt",
+            "sending mail": "versturen e-mails",
+            "sent mail": "mail verstuurd",
+            "finished": "klaar",
+
+            "skipped sending mail: no mail server configured": "sturen van mail overgeslagen: geen mailserver ingesteld",
+            "skipped sending mail: no e-mail addresses associated with account": "sturen van mail overgeslagen: geen mail adres bekend",
+
+            "network_error": "netwerkproblemen ontdekt, neem contact op met internet.nl",
+            "configuration_error": "configuratie onjuist ingesteld, neem contact op met internet.nl",
+            "timeout": "wachttijd vertreken, probeert opnieuw"
         }
     }
 }
