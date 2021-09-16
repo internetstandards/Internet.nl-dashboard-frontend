bf44d7cc8203808951add543eb62438ef4c98f21
stitch1 <elger.jonker@gmail.com>
2021-09-16 17:26:26 +0200

    add some methods to the tag filter

--- src/components/reports/ReportTagFilter.vue
+++ src/components/reports/ReportTagFilter.vue
@@ -1,6 +1,12 @@
 <style scoped>
+.firstbutton {
+  border-radius: 4px 0 0 4px !important;
+}
 .lastbutton {
   border-radius: 0 4px 4px 0 !important;
+}
+.intermediatebutton {
+  border-radius: 0 0 0 0 !important;
 }
 </style>
 <template>
@@ -8,14 +14,23 @@
     <beta-label />
     Dit rapport ondersteund tags, hierop kan worden gefilterd. Dit gebeurd live op de database, hierdoor kan het even duren voordat dit is toegepast.
     Meerdere tags selecteren betekent dat deze allemaal zijn toegepast per domein.
+
+    <loading :loading="loading" />
+
     <b-input-group>
-      <v-select :options="available_tags" v-model="selected_tags" multiple taggable style="width: 80%;" placeholder="-- filter by tag">
+            <b-input-group-prepend>
+        <b-button @click="load" variant="info" class="firstbutton">🔁 <span class="sr-only">Reload</span></b-button>
+        </b-input-group-prepend>
+      <v-select :options="available_tags" v-model="selected_tags" multiple taggable style="width: 70%;" placeholder="-- filter by tag">
         <template v-slot:option="option">
           <tag :value="option.label"/>
         </template>
       </v-select>
-      <b-input-group-append>
-        <b-button @click="apply" variant="info" class="lastbutton">Toepassen</b-button>
+
+        <b-input-group-append>
+
+          <b-button @click="apply" variant="info" class="intermediatebutton">✅ <span class="sr-only">Toepassen</span></b-button>
+        <b-button @click="clear" :disabled="selected_tags.length < 1" variant="info" class="lastbutton">❌ <span class="sr-only">Wis</span></b-button>
       </b-input-group-append>
     </b-input-group>
 
@@ -41,6 +56,7 @@ export default {
     return {
       available_tags: [],
       selected_tags: [],
+      loading: false,
     }
   },
   beforeDestroy() {
@@ -51,9 +67,16 @@ export default {
     urllist_id: {type: Number, required: true}
   },
   methods: {
+    clear() {
+      this.$store.commit("set_tags", []);
+      this.selected_tags = [];
+      this.$emit('tags_applied')
+    },
     load() {
+      this.loading = true;
       http.get(`data/urllist/tag/list/${this.urllist_id}/`).then(response => {
         this.available_tags = response.data
+        this.loading = false;
       });
     },
     apply() {
