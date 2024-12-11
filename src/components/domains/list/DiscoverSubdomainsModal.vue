82200406c68586cd5e7f0c51f9eaaf579942d991
stitch1 <elger.jonker@gmail.com>
2024-12-11 11:31:33 +0100

    incorporate changes of pull 33763fe1e3cf249d79f77be690e81aa07d0e6f88

--- src/components/domains/list/DiscoverSubdomainsModal.vue
+++ src/components/domains/list/DiscoverSubdomainsModal.vue
@@ -167,7 +167,7 @@ export default {
     find_suggestions: function () {
 
       this.loading_suggestions = true;
-      http.post('data/urllist/suggest-subdomains/', {domain: this.input_domain, period: this.period}).then(server_response => {
+      http.get('data/urllist/suggest-subdomains/', { params: {domain: this.input_domain, period: this.period}}).then(server_response => {
         if (server_response.data.length > 0) {
           // allow the top level domain also to be added / selected as a convenience.
           this.suggestions = [this.input_domain];
@@ -175,6 +175,7 @@ export default {
             this.suggestions.push(suggestion + "." + this.input_domain);
           })
         } else {
+          console.error("Failed loading suggestions.")
           this.suggestions = [];
         }
         this.loading_suggestions = false;
