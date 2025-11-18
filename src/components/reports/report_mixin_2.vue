d25b8e0a3bccdc95c0bc588ef3afe1214be39569
stitch1 <elger.jonker@gmail.com>
2025-11-18 18:30:54 +0100

    make it easier to enter share code, use post when share code is used

--- src/components/reports/report_mixin_2.vue
+++ src/components/reports/report_mixin_2.vue
@@ -59,34 +59,68 @@ export default {
 
         // A smaller response means faster load times, loading the reports is noticible in vue while the download is fast
         console.log(`Getting report id: ${report_ids[i]}`)
-        http.get(`${link}${report_ids[i]}`, post_data).then(response => {
-          // The report might be empty, because the wrong code has been sent:
 
-          if (response.data !== undefined && response.data !== "") {
-            console.log(`Retrieved report data: ${report_ids[i]}`)
+        if (stored_share_code) {
+          // todo: make this nicer, it does the same on post and get... But the report code needs to be posted as we
+          //  cannot have that parameter in a url for security reasons.
+           http.post(`${link}${report_ids[i]}`, post_data).then(response => {
+             // The report might be empty, because the wrong code has been sent:
 
-            // only add comparison data to the second report, because we want to quickly access urls of that one.
-            if (i > 0)
-              this.add_comparison_urls_to_report(response.data)
+             if (response.data !== undefined && response.data !== "") {
+               console.log(`Retrieved report data: ${report_ids[i]}`)
 
-            reports[i] = response.data;
-            // use destructuring to avoid copying a large object to memory and then ditching the large value
-            // this is much faster and not as memory-intensive.
-            // shallow_reports[i] = structuredClone(response.data);
-            // shallow_reports[i]['calculation'] = [];
-            shallow_reports[i] = this._objectWithoutProperties(response.data, ['calculation']);
+               // only add comparison data to the second report, because we want to quickly access urls of that one.
+               if (i > 0)
+                 this.add_comparison_urls_to_report(response.data)
 
-            // this.$forceUpdate()
-            console.log(`Set report data: ${report_ids[i]}`)
-          }
-          this.reports_to_load--;
+               reports[i] = response.data;
+               // use destructuring to avoid copying a large object to memory and then ditching the large value
+               // this is much faster and not as memory-intensive.
+               // shallow_reports[i] = structuredClone(response.data);
+               // shallow_reports[i]['calculation'] = [];
+               shallow_reports[i] = this._objectWithoutProperties(response.data, ['calculation']);
 
-          if (this.reports_to_load === 0 && reports.length > 0) {
-            this.reports = shallowRef(reports);
-            // these can be changed by the user
-            this.shallow_reports = shallow_reports;
-          }
-        });
+               // this.$forceUpdate()
+               console.log(`Set report data: ${report_ids[i]}`)
+             }
+             this.reports_to_load--;
+
+             if (this.reports_to_load === 0 && reports.length > 0) {
+               this.reports = shallowRef(reports);
+               // these can be changed by the user
+               this.shallow_reports = shallow_reports;
+             }
+           });
+        } else {
+          http.get(`${link}${report_ids[i]}`, post_data).then(response => {
+            // The report might be empty, because the wrong code has been sent:
+
+            if (response.data !== undefined && response.data !== "") {
+              console.log(`Retrieved report data: ${report_ids[i]}`)
+
+              // only add comparison data to the second report, because we want to quickly access urls of that one.
+              if (i > 0)
+                this.add_comparison_urls_to_report(response.data)
+
+              reports[i] = response.data;
+              // use destructuring to avoid copying a large object to memory and then ditching the large value
+              // this is much faster and not as memory-intensive.
+              // shallow_reports[i] = structuredClone(response.data);
+              // shallow_reports[i]['calculation'] = [];
+              shallow_reports[i] = this._objectWithoutProperties(response.data, ['calculation']);
+
+              // this.$forceUpdate()
+              console.log(`Set report data: ${report_ids[i]}`)
+            }
+            this.reports_to_load--;
+
+            if (this.reports_to_load === 0 && reports.length > 0) {
+              this.reports = shallowRef(reports);
+              // these can be changed by the user
+              this.shallow_reports = shallow_reports;
+            }
+          });
+        }
       }
     },
     // https://stackoverflow.com/questions/34698905/how-can-i-clone-a-javascript-object-except-for-one-key
