45eb5006a7322a8a6651518d0bb69c770378a622
stitch1 <elger.jonker@gmail.com>
2026-08-06 16:28:47 +0200

    better distribute donut charts

--- src/components/charts/donuts.vue
+++ src/components/charts/donuts.vue
@@ -1,23 +1,50 @@
 <!-- SPDX-License-Identifier: Apache-2.0 -->
+<style scoped>
+.donut-layout {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: center;
+}
+
+.donut-layout__item {
+  flex: 1 1 100%;
+  max-width: 300px;
+  min-width: 0;
+}
+
+@media (min-width: 576px) {
+  .donut-layout__item {
+    flex-basis: 260px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .donut-layout__item {
+    flex-basis: 165px;
+  }
+}
+</style>
+
 <template>
   <div>
     <div v-for="report in reports" :key="report.id">
       <template v-if="reports.length > 1">
         <h4 class="mb-4">#{{ report.id }} - {{ report.urllist_name }}</h4>
       </template>
-      <b-row cols-sm="2" cols-md="4" v-if="report['calculation']['statistics_per_issue_type']">
-        <b-col
-            v-for="item in visible_fields_from_scan_form(scan_methods[report.report_type === 'mail' ? 1 : 0])"
+      <div class="donut-layout" v-if="report['calculation']['statistics_per_issue_type']">
+        <div
+            class="donut-layout__item"
+            v-for="item in donut_items(report)"
             :key="item">
           <donut :height='300'
                  :tooltip="false"
                  :data="report['calculation']['statistics_per_issue_type'][item]"
                  :elements="['donut', 'subtitle']"
                  :title="item"
-                 v-if="report['calculation']['statistics_per_issue_type'][item]"
           />
-        </b-col>
-      </b-row>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -32,6 +59,13 @@ export default {
   components: {Donut},
   props: {
     reports: {type: Array, required: true}
+  },
+  methods: {
+    donut_items(report) {
+      const statistics = report.calculation?.statistics_per_issue_type ?? {}
+      const scanMethod = this.scan_methods[report.report_type === 'mail' ? 1 : 0]
+      return this.visible_fields_from_scan_form(scanMethod).filter((item) => statistics[item])
+    }
   }
 }
 </script>
